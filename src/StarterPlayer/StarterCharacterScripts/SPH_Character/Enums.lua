7514525426dd13b487e37a376f6a45a7a76f4f33
justin <jzou0417a@gmail.com>
2026-04-01 17:22:08 -0400

    Add input enums

---
+++ src/StarterPlayer/StarterCharacterScripts/SPH_Character/Enums.lua
@@ -0,0 +1,19 @@
+return {
+    Stance = {
+        STANDING = "stand",
+        CROUCHING = "crouch",
+        CRAWLING = "crawl",
+    },
+    Lean = {
+        LEFT = "left",
+        RIGHT = "right",
+        NONE = "none",
+    },
+    Intents = {
+        STANCE_UP = "ph_stanceup",
+        STANCE_DOWN = "ph_stancedown",
+        LEAN_LEFT = "ph_leanleft",
+        LEAN_RIGHT = "ph_leanright",
+        LEAN_NONE = "ph_leannone",
+    }
+}
