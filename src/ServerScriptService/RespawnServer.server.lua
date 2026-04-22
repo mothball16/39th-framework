a8a261c1b3acfddb43c8055e9c0e57b3e78519ff
justin <jzou0417a@gmail.com>
2026-04-22 11:26:21 -0400

    Remove unnecessary systems

--- src/ServerScriptService/RespawnServer.server.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local RespawnEvent = ReplicatedStorage:WaitForChild("RespawnPlayer")
-
-RespawnEvent.OnServerEvent:Connect(function(player)
-	if player then
-		player:LoadCharacter()
-	end
-end)
