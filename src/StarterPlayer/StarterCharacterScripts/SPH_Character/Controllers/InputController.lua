177acc9375dc160a4551398b3db3024d7ee82dd7
justin <jzou0417a@gmail.com>
2026-04-22 13:08:39 -0400

    Simplify inputcontroller methods

--- src/StarterPlayer/StarterCharacterScripts/SPH_Character/Controllers/InputController.lua
+++ src/StarterPlayer/StarterCharacterScripts/SPH_Character/Controllers/InputController.lua
@@ -82,38 +82,30 @@ function InputController.UnbindInput(...)
 	end
 end
 
-function InputController.BindAiming()
-	InputController.BindInput(Intents.HOLD_AIM, config.mobileButtons, config.gunInputPriority, unpack(config.aimGun))
-	ContextActionService:SetTitle(Intents.HOLD_AIM, "Aim")
-	ContextActionService:SetPosition(Intents.HOLD_AIM, UDim2.fromScale(0.24, 0.3))
-end
+----------------------------------------------------------------------------------------------------------------
 
-function InputController.UnbindAiming()
-	InputController.UnbindInput(Intents.HOLD_AIM)
-end
-
-function InputController.BindGunInputs(isFirstPerson)
+function InputController.BindGunInputs()
 	InputController
-		.BindInput(Intents.TRIGGER, config.mobileButtons, config.gunInputPriority, unpack(config.fireGun))
+		.BindInput(Intents.TRIGGER, false, config.gunInputPriority, unpack(config.fireGun))
 		.BindInput(Intents.DROP_GUN, false, config.gunInputPriority, unpack(config.dropKey))
-		.BindInput(Intents.RELOAD, config.mobileButtons, config.gunInputPriority, unpack(config.keyReload))
+		.BindInput(Intents.RELOAD, false, config.gunInputPriority, unpack(config.keyReload))
 		.BindInput(Intents.CHAMBER, false, config.gunInputPriority, unpack(config.keyChamber))
 		.BindInput(Intents.SWITCH_SIGHTS, false, config.gunInputPriority, unpack(config.sightSwitch))
 		.BindInput(Intents.FREELOOK, false, config.gunInputPriority, unpack(config.freeLook))
+		.BindInput(Intents.HOLD_AIM, config.mobileButtons, config.gunInputPriority, unpack(config.aimGun))
 
 		.BindInput(Intents.SWITCH_FIRE_MODE, false, config.gunInputPriority, unpack(config.switchFireMode))
 		.BindInput(Intents.TOGGLE_LASER, false, config.gunInputPriority, unpack(config.toggleLaser))
 		.BindInput(Intents.TOGGLE_FLASHLIGHT, false, config.gunInputPriority, unpack(config.toggleFlashlight))
+end
 
-	if isFirstPerson then
-		InputController.BindAiming()
-	end
-
-	ContextActionService:SetTitle(Intents.TRIGGER, "Fire")
-	ContextActionService:SetPosition(Intents.TRIGGER, UDim2.fromScale(0.3, 0.6))
-
-	ContextActionService:SetTitle(Intents.RELOAD, "Reload")
-	ContextActionService:SetPosition(Intents.RELOAD, UDim2.fromScale(0, 0.6))
+function InputController.BindCharacterInputs()
+	InputController
+		.BindInput(Intents.LEAN_LEFT, false, config.movementInputPriority, unpack(config.leanLeft))
+		.BindInput(Intents.LEAN_RIGHT, false, config.movementInputPriority, unpack(config.leanRight))
+		.BindInput(Intents.SPRINT, false, config.movementInputPriority, unpack(config.keySprint))
+		.BindInput(Intents.STANCE_DOWN, false, config.movementInputPriority, unpack(config.lowerStance))
+		.BindInput(Intents.STANCE_UP, false, config.movementInputPriority, unpack(config.raiseStance))
 end
 
 function InputController.UnbindGunInputs()
@@ -132,24 +124,6 @@ function InputController.UnbindGunInputs()
 		Intents.TOGGLE_LASER,
 		Intents.TOGGLE_FLASHLIGHT
 	)
-end
-
-function InputController.BindCharacterInputs()
-	InputController
-		.BindInput(Intents.LEAN_LEFT, false, config.movementInputPriority, unpack(config.leanLeft))
-		.BindInput(Intents.LEAN_RIGHT, false, config.movementInputPriority, unpack(config.leanRight))
-		.BindInput(Intents.SPRINT, false, config.movementInputPriority, unpack(config.keySprint))
-		.BindInput(Intents.STANCE_DOWN, false, config.movementInputPriority, unpack(config.lowerStance))
-		.BindInput(Intents.STANCE_UP, false, config.movementInputPriority, unpack(config.raiseStance))
-end
-
-function InputController.UnbindCharacterInputs()
-	InputController.UnbindInput(
-		Intents.LEAN_LEFT,
-		Intents.LEAN_RIGHT,
-		Intents.SPRINT,
-		Intents.STANCE_DOWN,
-		Intents.STANCE_UP)
 end
 
 UserInputService.InputChanged:Connect(function(input)
