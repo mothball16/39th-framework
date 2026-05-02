971194ba02bbcafafbf80b081f5a232bd3f4e372
justin <jzou0417a@gmail.com>
2026-05-02 15:16:09 -0400

    Update themes based on coolors palette

--- src/StarterPlayer/StarterPlayerScripts/Class_Player/UI/Theme.lua
+++ src/StarterPlayer/StarterPlayerScripts/Class_Player/UI/Theme.lua
@@ -5,15 +5,19 @@ local function CreateFont(family, weight, style)
     return f
 end
 
+-- https://coolors.co/0b1732-3556ac-fdfffc-c1292e-f1d302
 return {
-    Background = Color3.fromRGB(30, 30, 30),
-    BackgroundAlt = Color3.fromRGB(60, 60, 60),
-    AccentColor = Color3.fromRGB(0, 120, 215),
-    AccentColorAlt = Color3.fromRGB(32, 62, 87),
-    TextColor = Color3.fromRGB(255, 255, 255),
+    Background = Color3.fromHex("0B1732"),
+    BackgroundAlt = Color3.fromHex("122654"),
+    AccentColor = Color3.fromHex("3556AC"),
+    AccentColorAlt = Color3.fromHex("7388A6"),
+    ColorWarning = Color3.fromHex("f1d302"),
+    ColorError = Color3.fromHex("c1292e"),
+    TextColor = Color3.fromHex("fdfffc"),
+    TextColorDark = Color3.fromHex("122654"),
 
     fontH1 = CreateFont(Enum.Font.RobotoCondensed, Enum.FontWeight.Bold),
     fontH2 = CreateFont(Enum.Font.RobotoCondensed, Enum.FontWeight.SemiBold),
     fontH3 = CreateFont(Enum.Font.RobotoCondensed, Enum.FontWeight.Medium),
-    fontNormal = CreateFont(Enum.Font.Code, Enum.FontWeight.Regular),
+    fontNormal = CreateFont(Enum.Font.RobotoMono, Enum.FontWeight.Regular),
 }
