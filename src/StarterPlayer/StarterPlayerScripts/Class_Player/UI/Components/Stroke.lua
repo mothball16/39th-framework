8c8c071d443113142781b2ba0702369bd268fa7c
justin <jzou0417a@gmail.com>
2026-05-04 15:29:04 -0400

    Allow any props to be passed into stroke

--- src/StarterPlayer/StarterPlayerScripts/Class_Player/UI/Components/Stroke.lua
+++ src/StarterPlayer/StarterPlayerScripts/Class_Player/UI/Components/Stroke.lua
@@ -3,22 +3,13 @@ local Packages = ReplicatedStorage:WaitForChild("Packages")
 local Vide = require(Packages.Vide)
 local create = Vide.create
 
-return function(props: {
-	Color: Color3,
-	Thickness: number,
-	LineJoinMode: Enum.LineJoinMode,
-})
+return function(props)
 	return create "Frame"{
         Name = "Stroke",
         AnchorPoint = Vector2.new(0.5, 0.5),
         Position = UDim2.new(0.5, 0, 0.5, 0),
         Size = UDim2.new(1, -props.Thickness * 2, 1, -props.Thickness * 2),
         BackgroundTransparency = 1,
-        
-        create "UIStroke" {
-            Color = props.Color,
-            LineJoinMode = props.LineJoinMode,
-            Thickness = props.Thickness,
-        },
+        create "UIStroke" (props),
     }
 end
