1ae0c0d849f8870712b18ab7c6a593bde172b862
justin <jzou0417a@gmail.com>
2026-06-02 21:52:18 -0400

    Switch from warns to returning statuses

--- src/_Systems/ClassSystem/Class_Framework/Logic/StateActions.lua
+++ src/_Systems/ClassSystem/Class_Framework/Logic/StateActions.lua
@@ -16,7 +16,8 @@ local function _updateMapValue(atom, key, value)
 	end)
 end
 
-function StateActions.CreateFaction(state: State.State, config: Types.FactionConfig)
+function StateActions.CreateFaction(state: State.State, config: Types.FactionConfig): (boolean, string?)
+	local msg = ""
 	state.configByFactionId(function(previous)
 		local nextState = table.clone(previous)
 		nextState[config.ID] = config
@@ -25,11 +26,11 @@ function StateActions.CreateFaction(state: State.State, config: Types.FactionCon
 		for groupKey, groupConfig in pairs(config.Groups) do
 			if groupConfig.Default then
 				if groupConfig.Limit ~= math.huge then
-					warn(`faction {config.ID} group {groupKey} should have no limit - it is a default group`)
+					msg ..= `warning: faction {config.ID} group {groupKey} should have no limit - it is a default group\n`
 					groupConfig.Limit = math.huge
 				end
 				if groupConfig.Classes[1].AccessCheck then
-					warn(`the first class of default group {groupKey} of faction {config.ID} should not have an access check - the player can potentially resolve to no classes`)
+					msg ..= `warning: the first class of default group {groupKey} of faction {config.ID} should not have an access check - the player can potentially resolve to no classes\n`
 					groupConfig.Classes[1].AccessCheck = nil
 				end
 				nextState[config.ID].DefaultGroupKey = groupKey
@@ -39,10 +40,10 @@ function StateActions.CreateFaction(state: State.State, config: Types.FactionCon
 		return nextState
 	end)
 
-	return config
+	return true, msg
 end
 
-function StateActions.RemoveFaction(state: State.State, idToRemove: string)
+function StateActions.RemoveFaction(state: State.State, idToRemove: string): (boolean, string?)
 	state.configByFactionId(function(previous)
 		local nextState = table.clone(previous)
 		nextState[idToRemove] = nil
@@ -58,7 +59,7 @@ function StateActions.RemoveFaction(state: State.State, idToRemove: string)
 	end
 
 	if #affectedPlayers == 0 then
-		return
+		return true, `no players are affected by the removal of faction {idToRemove}`
 	end
 
 	-- is dirty, update reactively
@@ -76,55 +77,93 @@ function StateActions.RemoveFaction(state: State.State, idToRemove: string)
 		state.playerByGroupKey(nextplayerByGroupKey)
 		state.playerByClassId(nextplayerByClassId)
 	end)
+	return true, nil
 end
 
-function StateActions.SetPlayerFaction(state: State.State, userId: string, factionId: string)
-	-- if the faction id is the same as the previous, don't update
+function StateActions.SetPlayerFaction(state: State.State, userId: string, factionId: string): (boolean, string?)
+	if not state.configByFactionId()[factionId] then
+		return false, `denied: faction {factionId} is not a valid faction`
+	end
+
 	if state.playerByFactionId()[userId] == factionId then
-		return
+		return true, `ignored: faction id {factionId} is the same as the previous`
 	end
 
 	_updateMapValue(state.playerByFactionId, userId, factionId)
-	StateActions.SetPlayerToDefaultGroupClass(state, userId, factionId)
+	local success, msg = StateActions.SetPlayerToDefaultGroupClass(state, userId, factionId)
+	return success, msg
 end
 
-function StateActions.SetPlayerGroupClass(state: State.State, userId: string, groupKey: string?, classId: string?)
+function StateActions.SetPlayerGroupClass(state: State.State, userId: string, groupKey: string?, classId: string?): (boolean, string?)
 	-- either intentionally or accidentally empty, remove everything cause
 	-- it will brick the classes otherwise
 	if not groupKey or not classId then
 		groupKey = nil
 		classId = nil
-	end
+	else
+		local playerFactionId = state.playerByFactionId()[userId]
+		local factionConfig = state.configByFactionId()[playerFactionId]
+		if not playerFactionId then
+			return false, `denied: player {userId} is not assigned to a faction`
+		end
 
-	-- if the group key and class id are the same as the previous, don't update
-	if state.playerByGroupKey()[userId] == groupKey and state.playerByClassId()[userId] == classId then
-		return
+		if not factionConfig then
+			return false, `denied: faction {playerFactionId} is not a valid faction`
+		end
+	
+		if state.playerByGroupKey()[userId] == groupKey and state.playerByClassId()[userId] == classId then
+			return true, `ignored: group key {groupKey} and class id {classId} are the same as the previous`
+		end
+	
+		if not factionConfig.Groups[groupKey] then
+			return false, `denied: group key {groupKey} is not a valid group of faction {playerFactionId}`
+		end
+
+		local foundConfig: Types.ClassDescriptor?
+		for _, classConfig in ipairs(factionConfig.Groups[groupKey].Classes) do
+			if classConfig.Id == classId then
+				foundConfig = classConfig
+				break
+			end
+		end
+
+		if not foundConfig then
+			return false, `denied: class id {classId} is not a valid class of group {groupKey} of faction {playerFactionId}`
+		end
+
+		if foundConfig.AccessCheck and not foundConfig.AccessCheck(userId) then
+			return false, `denied: player {userId} fails the access check for class {classId}`
+		end
 	end
 
 	Charm.batch(function()
 		_updateMapValue(state.playerByGroupKey, userId, groupKey)
 		_updateMapValue(state.playerByClassId, userId, classId)
 	end)
+	return true, nil
 end
 
-function StateActions.RemovePlayerFaction(state: State.State, userId: string)
-	_updateMapValue(state.playerByFactionId, userId, nil)
-	StateActions.RemovePlayerGroupClass(state, userId)
-end
-
-function StateActions.RemovePlayerGroupClass(state: State.State, userId: string)
+function StateActions.RemovePlayerGroupClass(state: State.State, userId: string): (boolean, string?)
 	return StateActions.SetPlayerGroupClass(state, userId, nil, nil)
 end
 
-function StateActions.SetPlayerToDefaultGroupClass(state: State.State, userId: string, factionId: string)
+
+function StateActions.RemovePlayerFaction(state: State.State, userId: string): (boolean, string?)
+	_updateMapValue(state.playerByFactionId, userId, nil)
+	return StateActions.RemovePlayerGroupClass(state, userId)
+end
+
+
+function StateActions.SetPlayerToDefaultGroupClass(state: State.State, userId: string, factionId: string): (boolean, string?)
 	local factionConfig = state.configByFactionId()[factionId]
+	local msg = ""
 	if not factionConfig then
-		return
+		return false, `denied: faction {factionId} is not a valid faction`
 	end
 
 	local defaultGroupKey = factionConfig.DefaultGroupKey
 	if not defaultGroupKey then
-		warn(`faction {factionId} has no default group`)
+		msg ..= `faction {factionId} has no default group\n`
 		defaultGroupKey = next(factionConfig.Groups)
 	end
 
@@ -132,13 +171,14 @@ function StateActions.SetPlayerToDefaultGroupClass(state: State.State, userId: s
 	local classId = groupConfig.Classes[1].Id
 
 	if not classId then
-		error(`faction {factionId} has no class id!`)
+		return false, `error: faction {factionId} has no class ids!`
 	end
 
 	Charm.batch(function()
 		_updateMapValue(state.playerByGroupKey, userId, defaultGroupKey)
 		_updateMapValue(state.playerByClassId, userId, classId)
 	end)
+	return true, msg
 end
 
 
