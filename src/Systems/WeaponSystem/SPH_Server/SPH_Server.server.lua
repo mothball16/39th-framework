d025d9177fd2a4766e40707302d38346db7f7772
justin <jzou0417a@gmail.com>
2026-06-18 21:54:05 -0400

    Revert "Remove unfunny death sounds"
    This reverts commit 40ed6cefef93ddb8d892cdbc3c59570bed8020d5.

--- src/Systems/WeaponSystem/SPH_Server/SPH_Server.server.lua
+++ src/Systems/WeaponSystem/SPH_Server/SPH_Server.server.lua
@@ -580,6 +580,11 @@ local function initializePlayerLifecycle()
 						end
 					end
 				end
+				local deathSounds = assets.Sounds.Death:GetChildren()
+				local newSound = deathSounds[math.random(#deathSounds)]:Clone()
+				newSound.Parent = hrp
+				newSound:Play()
+				debris:AddItem(newSound, newSound.TimeLength)
 			end)
 			newPlayer.Backpack.ChildAdded:Connect(function(child)
 				checkTool(newPlayer, child)
